05c0a08cb5e66db4dee070f9b44943d6a5f9a1ec
star <triptibarnwal0@gmail.com>
2025-04-02 23:00:30 +0530

    Updated start.sh with updated version

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-apt update && apt install -y tesseract-ocr libc-bin
-export LD_LIBRARY_PATH=/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+apt update && apt install -y tesseract-ocr libc-bin wget
+
+# Download and install GLIBC 2.38
+mkdir /glibc
+cd /glibc
+wget http://ftp.gnu.org/gnu/libc/glibc-2.38.tar.gz
+tar -xvzf glibc-2.38.tar.gz
+cd glibc-2.38
+mkdir build
+cd build
+../configure --prefix=/opt/glibc-2.38
+make -j$(nproc)
+make install
+
+# Use the new GLIBC version
+export LD_LIBRARY_PATH=/opt/glibc-2.38/lib:$LD_LIBRARY_PATH
 gunicorn -w 4 -b 0.0.0.0:8080 auth:app
